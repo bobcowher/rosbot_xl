fdfa69b686147c2fe43a4efd0d6703045f783859
Robert Cowher <bobcowher@gmail.com>
2025-06-01 09:58:35 -0500

    Updated to include the appropriate build_with_compile_commands.sh script

--- scripts/startsim.sh
+++ scripts/startsim.sh
@@ -1,3 +1,1 @@
-#!/bin/bash
-
-ros2 launch rosbot_xl_gazebo simulation.launch.py
+ros2 launch rosbot_xl_gazebo simulation.launch.py world:=install/husarion_gz_worlds/share/husarion_gz_worlds/worlds/empty_with_plugins.sdf
