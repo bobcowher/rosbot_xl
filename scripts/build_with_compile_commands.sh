6765fb3cee9687c436ae176c73878e6db91f96c6
Robert Cowher <bobcowher@gmail.com>
2025-06-07 21:08:58 -0500

    Updated to include the latest build scripts

--- scripts/build_with_compile_commands.sh
+++ scripts/build_with_compile_commands.sh
@@ -4,6 +4,7 @@ colcon build --cmake-args -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
 # Copy the workspace and compile commands json.
 cp -pr /ros2_ws/build /ros2_ws_mirror/
 cp -pr /ros2_ws/install /ros2_ws_mirror/
+cp -pr /ros2_ws/src /ros2_ws_mirror/
 cp -pr /ros2_ws/build/compile_commands.json /ros2_ws_mirror/
 
 # Copy the humble installation
